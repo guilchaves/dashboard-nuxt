bf27c4fb6d9e55b7fb18f1a0e7fd8681ec6fcb2c
guilherme chaves <guilherme.chaves@simplasolucoes.com.br>
2024-09-26 16:21:00 -0300

    feat: add transaction page

---
+++ pages/transactions.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header>
+      this is my header
+    </header>
+    <main>
+      this is my main
+      <section>
+
+      </section>
+    </main>
+    <footer>
+      this is my footer
+    </footer>
+  </div>
+</template>
